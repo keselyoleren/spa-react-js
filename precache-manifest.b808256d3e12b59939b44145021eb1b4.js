7ace4d891b8edc8ad5efd74652ce2504effe7053
M Hadi Sasmito <keselyoleren@gmail.com>
2020-03-22 22:42:02 +0700

    Updates

---
+++ precache-manifest.b808256d3e12b59939b44145021eb1b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55de6323345678f1ceb54d20a848f646",
+    "url": "/spa-react-js/index.html"
+  },
+  {
+    "revision": "a6d34751c661f0e411b5",
+    "url": "/spa-react-js/static/css/main.dd05d1ef.chunk.css"
+  },
+  {
+    "revision": "595a1e92332e261186ef",
+    "url": "/spa-react-js/static/js/2.95d856fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spa-react-js/static/js/2.95d856fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d34751c661f0e411b5",
+    "url": "/spa-react-js/static/js/main.8117ae11.chunk.js"
+  },
+  {
+    "revision": "4f81552f72d0a38936bd",
+    "url": "/spa-react-js/static/js/runtime-main.a8ac4d4a.js"
+  }
+]);
